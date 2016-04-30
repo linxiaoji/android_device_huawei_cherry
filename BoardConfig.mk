f6ba6461470951ad3987e766bc1e3714f250dbfe
Muhammad Fahad Baig <muhammad.fahad.baig@outlook.com>
2016-04-30 10:06:49 +0500

    Initial Device Tree

---
+++ BoardConfig.mk
@@ -0,0 +1,163 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# 64-bit
+ANDROID_64=true
+TARGET_USES_64_BIT_BINDER                   := true
+TARGET_SUPPORTS_32_BIT_APPS                 := true
+TARGET_SUPPORTS_64_BIT_APPS                 := true
+
+# Primary Arch
+TARGET_ARCH                                 := arm64
+TARGET_ARCH_VARIANT                         := armv8-a
+TARGET_CPU_VARIANT                          := cortex-a53
+TARGET_CPU_ABI                              := arm64-v8a
+TARGET_CPU_SMP                              := true
+
+# Secondary Arch
+TARGET_2ND_ARCH                             := arm
+TARGET_2ND_ARCH_VARIANT                     := armv7-a-neon
+TARGET_2ND_CPU_VARIANT                      := cortex-a53
+TARGET_2ND_CPU_ABI                          := armeabi-v7a
+TARGET_2ND_CPU_ABI2                         := armeabi
+
+# Audio
+BOARD_USES_GENERIC_AUDIO                    := false
+BOARD_USES_ALSA_AUDIO                       := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT                     := true
+
+# Libc extensions
+BOARD_PROVIDES_ADDITIONAL_BIONIC_STATIC_LIBS += libc_huawei_symbols
+
+# Board
+BOARD_HAS_LARGE_FILESYSTEM                  := true
+BOARD_HAS_NO_SELECT_BUTTON                  := true
+BOARD_VENDOR                                := huawei
+BOARD_VENDOR_PLATFORM                       := hi6210sft
+HISI_TARGET_PRODUCT                         := hi6210sft
+TARGET_NO_BOOTLOADER                        := true
+TARGET_NO_KERNEL                            := false
+TARGET_NO_RECOVERY                          := false
+TARGET_BOARD_PLATFORM                       := hi6210sft
+TARGET_BOOTLOADER_BOARD_NAME                := hi6210sft
+TARGET_SOC                                  := kirin620
+TARGET_USES_HISI_DTIMAGE                    := true
+
+# Graphics and Hardware Acceleration
+ANDROID_ENABLE_RENDERSCRIPT                 := true
+BOARD_EGL_CFG                               := device/huawei/hi6210sft/egl.cfg
+TARGET_HARDWARE_3D                          := true
+USE_OPENGL_RENDERER                         := true
+
+# Generic Broadcom Wifi and Hostapd
+BOARD_WLAN_DEVICE                           := bcmdhd
+BOARD_WLAN_DEVICE_REV                       := bcm4343
+WPA_SUPPLICANT_VERSION                      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER                 := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB            := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER                        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB                   := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM                   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA                     := "/system/vendor/firmware/fw_bcm4343s_hw.bin"
+WIFI_DRIVER_FW_PATH_AP                      := "/system/vendor/firmware/fw_bcm4343s_apsta_hw.bin"
+WIFI_DRIVER_FW_PATH_P2P                     := "/system/vendor/firmware/fw_bcm4343s_p2p_hw.bin"
+WIFI_BAND                                   := 802_11_ABG
+CONFIG_DRIVER_NL80211                       := y
+CONFIG_DRIVER_WEXT                          := y
+
+# Bluetooth
+BOARD_BLUEDROID_VENDOR_CONF                 := device/huawei/hi6210sft/bluetooth/vnd_h60.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/huawei/hi6210sft/bluetooth
+BOARD_HAVE_BLUETOOTH                        := true
+BOARD_HAVE_BLUETOOTH_BCM                    := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE                        := console=ttyAMA3,115200 androidboot.console=ttyAMA3 firmware_class.path=/system/vendor/firmware hisi_dma_print=0 vmalloc=384M maxcpus=8 coherent_pool=512K no_irq_affinity loglevel=7 androidboot.selinux=permissive enforcing=0 ate_enable=true
+BOARD_KERNEL_BASE                           := 0x07478000
+TARGET_KERNEL_SOURCE                        := kernel/
+TARGET_KERNEL_ARCH                          := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX          := aarch64-linux-android-
+TARGET_KERNEL_HEADER_ARCH                   := arm64
+TARGET_KERNEL_CONFIG                        := hisi_hi6210sft_defconfig
+TARGET_USES_UNCOMPRESSED_KERNEL             := true
+BOARD_KERNEL_IMAGE_NAME                     := Image
+BOARD_KERNEL_PAGESIZE                       := 2048
+BOARD_RAMDISK_OFFSET                        := 0x07b88000
+BOARD_KERNEL_OFFSET                         := 0x00008000
+BOARD_TAGS_OFFSET                           := 0x02988000
+BUILD_KERNEL_MODULES                         = true
+
+# Kernel (Fallback): Uncomment the line if the sources are not present or a prebuilt kernel is to be used.
+#TARGET_KERNEL_PREBUILT := device/huawei/hi6210sft/kernel
+
+# Camera
+USE_CAMERA_STUB                             := true
+BOARD_CAMERA_HAVE_ISO                       := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE              := 25165824
+BOARD_RECOVERYIMAGE_PARTITION_SIZE          := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE            := 2550136832
+BOARD_USERDATAIMAGE_PARTITION_SIZE          := 4064280576
+BOARD_FLASH_BLOCK_SIZE                      := 131072
+TARGET_USERIMAGES_USE_EXT4                  := true
+
+# Display
+TARGET_USE_PAN_DISPLAY                      := true
+DEVICE_SCREEN_HEIGHT                        := 1280
+DEVICE_SCREEN_WIDTH                         := 720
+
+# TeamWin Recovery Project
+RECOVERY_VARIANT                            := twrp
+TW_THEME                                    := portrait_hdpi
+TW_BRIGHTNESS_PATH                          := "/sys/class/leds/lcd_backlight0/brightness"
+TW_CUSTOM_BATTERY_PATH                      := "/sys/devices/battery.0/power_supply/Battery"
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID      := true
+TW_MAX_BRIGHTNESS                           := 255
+RECOVERY_GRAPHICS_USE_LINELENGTH            := true
+RECOVERY_SDCARD_ON_DATA                     := true
+BOARD_HAS_LARGE_FILESYSTEM                  := true
+BOARD_RECOVERY_NEEDS_FBIOPAN_DISPLAY        := true
+
+# Board RIL
+BOARD_RIL_CLASS                             := ../../../device/huawei/hi6210sft/ril
+
+# Boot animation
+TARGET_BOOTANIMATION_PRELOAD                := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE          := true
+
+# Healthd
+BOARD_HAL_STATIC_LIBRARIES += libhealthd.hi6210sft
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
+# Security Enhanced Linux
+BOARD_SEPOLICY_DIRS                         := device/huawei/hi6210sft/sepolicy
+BOARD_SEPOLICY_UNION += \
+        file_contexts \
+        installd.de
+
+# CPUSETS Feature
+ENABLE_CPUSETS                              := true
+
---
+++ device.mk
@@ -0,0 +1,209 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Defining Local Path
+LOCAL_PATH := device/huawei/hi6210sft
+
+# Adjust the dalvik heap to be appropriate for a phone
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Copy preboot binaries
+PRE_BOOT_FILES := isp.bin ons.bin
+PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
+        $(LOCAL_PATH)/preboot/isp.bin:system/isp.bin \
+        $(LOCAL_PATH)/preboot/ons.bin:system/ons.bin)
+
+# Set custom settings
+DEVICE_PACKAGE_OVERLAYS := device/huawei/hi6210sft/overlay
+
+# Add openssh support for remote debugging and job submission
+PRODUCT_PACKAGES += ssh sftp scp sshd ssh-keygen sshd_config start-ssh uim wpa_supplicant
+
+# Build and run only ART
+PRODUCT_RUNTIMES := runtime_libart_default
+
+# Build BT a2dp audio HAL
+PRODUCT_PACKAGES += audio.a2dp.default
+
+# Needed to sync the system clock with the RTC clock
+PRODUCT_PACKAGES += hwclock
+
+# Include USB speed switch App
+PRODUCT_PACKAGES += UsbSpeedSwitch
+
+# Build libion for new double-buffering HDLCD driver
+PRODUCT_PACKAGES += libion
+
+# Build gatord daemon for DS-5/Streamline
+PRODUCT_PACKAGES += gatord
+
+# Build gralloc for Juno
+PRODUCT_PACKAGES += gralloc.hi6210sft gralloc.default
+
+# Include ION tests
+PRODUCT_PACKAGES += \
+        iontest \
+        ion-unit-test
+
+# Build audio libraries
+PRODUCT_PACKAGES += \
+	audio.primary.default \
+	audio_policy.stub \
+	audio.usb.default \
+	audio.r_submix.default \
+	sound_trigger.primary.hi6210sft \
+	libaudioutils \
+	libtinyalsa \
+	tinyplay \
+	tinycap \
+	tinymix \
+	tinypcminfo
+
+# Set zygote config
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
+PRODUCT_PROPERTY_OVERRIDES += \
+        debug.sf.no_hw_vsync=1 \
+        ro.secure=0 \
+        ro.adb.secure=0
+PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
+
+# Graphics
+PRODUCT_PACKAGES += libGLES_android
+
+# WiFi and Bluetooth
+PRODUCT_PACKAGES += \
+        libwpa_client \
+	dhcpcd.conf \
+	hostapd \
+	wpa_supplicant \
+	wpa_supplicant.conf
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0 \
+        wifi.supplicant_scan_interval=15
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/bt-wifi-firmware-util/TIInit_11.8.32.bts:system/etc/firmware/TIInit_11.8.32.bts \
+        $(LOCAL_PATH)/bt-wifi-firmware-util/wl18xx-fw-4.bin:system/etc/firmware/ti-connectivity/wl18xx-fw-4.bin \
+        $(LOCAL_PATH)/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+        $(LOCAL_PATH)/bt-wifi-firmware-util/TIInit_11.8.32.bts:system/etc/firmware/ti-connectivity/TIInit_11.8.32.bts
+
+# Permissions 
+PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
+        frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+        frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+        frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+        frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+        frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+        frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+        frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+        frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+        frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+        frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+        frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+        frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+        frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+        frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+        frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+        frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+        frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+        frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+        packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# Include BT modules
+$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
+
+# Camera
+PRODUCT_PACKAGES += camera.hi6210sft camera.default
+
+# AAPT high-density artwork where available
+PRODUCT_AAPT_CONFIG      := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+	audioril.lib=libhuawei-audio-ril.so \
+	ro.telephony.ril_class=HuaweiRIL \
+	ro.telephony.default_network=9 \
+	telephony.lteOnCdmaDevice=0 \
+	telephony.lteOnGsmDevice=0
+
+# Additional packages
+PRODUCT_PACKAGES += \
+        Torch \
+        com.android.future.usb.accessory
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        persist.sys.usb.config=mtp
+
+# USB OTG support
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.isUsbOtgEnabled=true
+
+# File System
+PRODUCT_PACKAGES += \
+	make_ext4fs \
+	setup_fs
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
+	$(LOCAL_PATH)/ramdisk/init.recovery.hi6210sft.rc:root/init.recovery.hi6210sft.rc \
+	$(LOCAL_PATH)/ramdisk/init.hi6210sft.rc:root/init.hi6210sft.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ramdisk/sbin/emmc_partation:root/sbin/emmc_partation \
+	$(LOCAL_PATH)/ramdisk/sbin/hdbd:root/sbin/hdbd \
+	$(LOCAL_PATH)/ramdisk/sbin/logctl_service:root/sbin/logctl_service \
+	$(LOCAL_PATH)/ramdisk/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server \
+	$(LOCAL_PATH)/ramdisk/sbin/teecd:root/sbin/teecd
+
+# Time Zone data
+PRODUCT_COPY_FILES += \
+        bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+# Security Enhanced Linux
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.boot.selinux=0
+ADDITIONAL_DEFAULT_PROPERTIES      += ro.boot.selinux=0
+
+# Inherit from Non Opensource Blobs
+$(call inherit-product, vendor/huawei/hi6210sft/vendor.mk)
+
+# Copy Audio Policies and Configurations
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+	$(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Copy GPS Configurations
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+	$(LOCAL_PATH)/gps/gpsconfig.xml:system/etc/gpsconfig.xml
+
+# Copy CPU and Thermal Configurations
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/cpu_thermal/normal_cpu_policy.xml:system/etc/normal_cpu_policy.xml \
+	$(LOCAL_PATH)/cpu_thermal/performance_cpu_policy.xml:system/etc/performance_cpu_policy.xml \
+	$(LOCAL_PATH)/cpu_thermal/powermonitor_config.xml:system/etc/powermonitor_config.xml \
+	$(LOCAL_PATH)/cpu_thermal/pwrpof.xml:system/etc/pwrprf.xml \
+	$(LOCAL_PATH)/cpu_thermal/thermald.xml:system/etc/thermald.xml \
+	$(LOCAL_PATH)/cpu_thermal/thermald_performance.xml:system/etc/thermald_performance.xml
+
+# Copy Media Profiles and Configurations
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
