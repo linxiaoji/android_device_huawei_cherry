cbadda6bbf97ead89a6ed92055a364e079dfb7bf
Kazakov Stepan <kazakov1999stepan1948.12@gmail.com>
2016-06-27 01:21:22 +0700

    64bit

--- cm.mk
+++ cm.mk
@@ -17,6 +17,9 @@
 # Inherit some common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# It's 64-bit product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
